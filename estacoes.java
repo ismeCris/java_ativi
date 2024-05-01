b373b50b564ac760646907abf44bd7129936168f
Cristiely  Martins <104570782+ismycris@users.noreply.github.com>
2024-05-01 12:13:32 -0300

    Create estacoes.java

---
+++ estacoes.java
@@ -0,0 +1,39 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Estacoes {
+	public static void main(String[]args) {
+		int mes;
+		
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Digite o mes( 1 a 12):");
+		mes = leitor.nextInt();
+		
+		switch(mes) {
+		case 12:
+		case 1:
+		case 2:
+			System.out.println(" primavera");
+			break;
+		case 3:
+		case 4:
+		case 5:
+			System.out.println(" outono");
+			break;
+		case 6:
+		case 7:
+		case 8:
+			System.out.println(" inverno");
+			break;
+		case 9:
+		case 10:
+		case 11:
+			System.out.println(" verao");
+			break;
+		}
+		
+		
+	}
+
+}
